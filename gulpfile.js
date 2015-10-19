f91cf1d65f212bd3a4e8eab2583e76f57a52fc1e
858683682@qq.com <858683682@qq.com>
2015-10-19 18:00:13 +0800

    增加webpack处理器
    增加各种loader

--- gulpfile.js
+++ gulpfile.js
@@ -25,15 +25,10 @@ var gulp = require('gulp'),
 gulp.task('styles', function() {
     gulp.src(['./src/css/demo.scss'])
         .pipe(autoprefix())
-        //.pipe(sourcemaps.init({loadMaps: true}))
         .pipe(sass())
-        //.pipe(sourcemaps.write('.',{sourceMappingURLPrefix: './build/styles'}))
         .pipe(minifycss())
         .pipe(rename('all.min.css'))
-        .pipe(rev())
         .pipe(gulp.dest('./dist/css'))
-        .pipe(rev.manifest())
-        .pipe(gulp.dest('dist'));
 });
 
 gulp.task('scripts', function() {
@@ -46,25 +41,8 @@ gulp.task('scripts', function() {
         ]))
         .pipe(concat('all-a.js'))
         .pipe(rename('all.min.js'))
-        .pipe(uglify())
-        .pipe(strip ())
-        .pipe(rev())
+        // .pipe(uglify())
         .pipe(gulp.dest('./dist/js'))
-        .pipe(rev.manifest())
-        .pipe(gulp.dest('./rev'));
-});
-
-
-gulp.task('usemin', function() {
-    return gulp.src('./*.html')
-        .pipe(usemin({
-            css: [ rev() ],
-            //html: [ minifyHtml({ empty: true }) ],
-            js: [  rev() ],
-            inlinejs: [ uglify() ],
-            inlinecss: [ 'concat' ]
-        }))
-        .pipe(gulp.dest('dist/'));
 });
 
 
@@ -73,5 +51,5 @@ gulp.task('default',function(){
     gulp.src(['./dist/'])
         .pipe(clean());
 
-    gulp.run('styles','scripts','usemin');
+    gulp.run('styles','scripts');
 });
