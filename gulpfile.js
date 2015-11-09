f63b64e603be8fbfc5c823503df7876c6056790f
858683682@qq.com <858683682@qq.com>
2015-11-09 13:46:52 +0800

    修改错误

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,12 @@
-
 // include gulp
 var gulp = require('gulp'),
 
-// include plug-ins
+    // include plug-ins
     clean = require('gulp-clean'),
     autoprefix = require('gulp-autoprefixer'),
     sass = require('gulp-sass'),
     sourcemaps = require('gulp-sourcemaps'),
-    strip  = require('gulp-strip-comments'),
+    strip = require('gulp-strip-comments'),
     jshint = require('gulp-jshint'),
     concat = require('gulp-concat'),
     uglify = require('gulp-uglify'),
@@ -20,14 +19,51 @@ var gulp = require('gulp'),
     order = require("gulp-order");
 
 
+
+gulp.task('dev', ['clean'], function() {
+    gulp.start([
+        'dev:js',
+        'dev:css'
+    ]);
+});
+
+
+gulp.task('clean',function(){
+    gulp.src(['./dist/'])
+        .pipe(clean());
+})
+
+
+gulp.task('dev:css',function(){
+       gulp.src(['./src/css/demo.scss'])
+        .pipe(autoprefix())
+        .pipe(sass())
+        .pipe(rename('all.css'))
+        .pipe(gulp.dest('./dist/css'))
+})
+
+
+gulp.task('dev:js', function () {
+    gulp.src('./src/*.js')
+        // .pipe(gulp.dest('dist/js'))
+        .pipe(rename('all.js'))
+        // .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'));
+});
+
+
+
+
+
+
+
 // JS hint task
 
 gulp.task('styles', function() {
     gulp.src(['./src/css/demo.scss'])
         .pipe(autoprefix())
         .pipe(sass())
-        .pipe(minifycss())
-        .pipe(rename('all.min.css'))
+        .pipe(rename('all.css'))
         .pipe(gulp.dest('./dist/css'))
 });
 
@@ -40,16 +76,16 @@ gulp.task('scripts', function() {
             'demo.js'
         ]))
         .pipe(concat('all-a.js'))
-        .pipe(rename('all.min.js'))
+        .pipe(rename('all.js'))
         // .pipe(uglify())
         .pipe(gulp.dest('./dist/js'))
 });
 
 
 
-gulp.task('default',function(){
+gulp.task('default', function() {
     gulp.src(['./dist/'])
         .pipe(clean());
 
-    gulp.run('styles','scripts');
+    gulp.run('styles', 'scripts');
 });
